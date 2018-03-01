8a6ec4ff28872fd007f6f5c0e454823d0132bfaf
ymz <ymz@yangminzidembp.lan>
2018-03-01 23:39:10 +0800

    init

---
+++ src/main/java/com/loyal/ymz/App.java
@@ -0,0 +1,13 @@
+package com.loyal.ymz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
